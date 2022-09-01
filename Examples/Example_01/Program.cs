2b07a4bfe4b6ef177e21bac73850142a32c4470a
ilnar <ilnar-sov@mail.ru>
2022-09-01 22:31:24 +0300

    Семинар 9

---
+++ Examples/Example_01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать натуральные числа от 1 до N, N задан
+
+string Numbers(int a, int b)
+{
+    if(a<=b) return $"{a} " + Numbers(a+1, b);
+    else return String.Empty;
+}
+Console.WriteLine(Numbers(1,10));
---
+++ Examples/Example_02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму цифр числа
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SplitNumber(int number)
+{
+    if(number == 0) return 0;
+    else return number % 10 + SplitNumber(number / 10); //% остаток от деления на 10 берёт последнюю цифру
+}
+
+int number = InputNumber("Введите число: ");
+Console.WriteLine(SplitNumber(number));
+
