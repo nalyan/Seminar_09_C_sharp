1fac9d8f52745061cbc0906c6d4ee6f7ef2dbf01
nalyan <ilnar-sov@mail.ru>
2022-09-10 15:11:09 +0300

    Практические задания к семинару 9

---
+++ Practical tasks/task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+int InputNumber (string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int NaturalNumbers(int m, int n)
+{
+    Console.Write($"{m} ");
+    if(m==n) return n;
+    else if (m<n) return NaturalNumbers(m+1, n);
+    else return NaturalNumbers(m-1, n);
+}
+
+Console.Clear();
+int M = InputNumber("Введите M: ");
+int N = InputNumber("Введите N: ");
+NaturalNumbers(M,N);
