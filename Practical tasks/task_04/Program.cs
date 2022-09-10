49a93f8ab89588eb34a08b2d67e9e1381b703f13
ilnar <ilnar-sov@mail.ru>
2022-09-10 15:52:05 +0300

    Практические задания к семинару 9

---
+++ Practical tasks/task_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// 4. Найти сумму цифр числа
+
+int InputNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int SumNumbers(int n)
+{
+    if(n/10 < 1) return n;
+    else return n%10 + SumNumbers(n/10);
+}
+Console.Clear();
+int n = InputNumber("Введите число: ");
+Console.WriteLine(SumNumbers(n));
