90e6b3ae3498c058de84ec4827c17135390cbad9
nalyan <ilnar-sov@mail.ru>
2022-09-10 15:10:57 +0300

    Практические задания к семинару 9

---
+++ Practical tasks/task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Показать натуральные числа от N до 1, N задано
+
+int InputNumber (string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int NumbersLine(int n)
+{
+    Console.Write($"{n} ");
+    if(n==1) return 1;
+    else return NumbersLine(n-1);
+}
+
+Console.Clear();
+int n = InputNumber("Введите число: ");
+NumbersLine(n);
