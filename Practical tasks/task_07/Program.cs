d9e298dc7fb6c46916dc7ded5f8e4746ddb755d6
ilnar <ilnar-sov@mail.ru>
2022-09-10 23:14:25 +0300

    Практические задания к семинару 9

---
+++ Practical tasks/task_07/Program.cs
@@ -0,0 +1,30 @@
+﻿// 7. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+int InputNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void Sum(int number1, int number2, int n)
+{
+    void SumPart(int number1, int number2, int n)
+    {
+        int numberEnd = number1+number2;
+        if(n>0)
+        {
+            Console.Write($"{numberEnd} ");
+            number1 = number2;
+            number2 = numberEnd;
+            SumPart(number1, number2, n-1);
+        }
+    }
+    Console.Write($"{number1} {number2} ");
+    SumPart(number1, number2, n-2);
+}
+
+Console.Clear();
+int number1 = InputNumber("Введите число 1: ");
+int number2 = InputNumber("Введите число 2: ");
+int n = InputNumber("Введите количество чисел последовательности: ");
+Sum(number1, number2, n);
