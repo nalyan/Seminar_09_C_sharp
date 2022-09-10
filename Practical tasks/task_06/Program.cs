7439a2071fb8cd8e2846132869119275120100aa
ilnar <ilnar-sov@mail.ru>
2022-09-10 18:42:09 +0300

    Практические задания к семинару 9

---
+++ Practical tasks/task_06/Program.cs
@@ -0,0 +1,18 @@
+﻿// 6. Написать программу возведения числа А в целую стень B
+
+int InputNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int Degree(int a, int b)
+{
+    int count;
+    if(b==1) return a;
+    else return Degree(a, b-1)*a;
+}
+Console.Clear();
+int a = InputNumber("Введите число: ");
+int b = InputNumber("Введите степень числа: ");
+Console.WriteLine(Degree(a, b));
